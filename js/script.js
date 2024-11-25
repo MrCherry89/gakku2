624df8bf8ae641a9dd0cae525c23b2fb3e1d763b
MrCherry89 <abgarnati@yandex.ru>
2024-11-25 04:47:03 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -22,4 +22,47 @@ $(document).ready(function () {
       },
     ],
   });
+
+  $(".planning-solutions-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    centerMode: true,
+    centerPadding: 0,
+    variableWidth: true,
+    focusOnSelect: true,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 1800,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          variableWidth: false,
+          focusOnSelect: false,
+        },
+      },
+    ],
+  });
+
+  const tabButtons = document.querySelectorAll(".tab-button");
+  const tabPanes = document.querySelectorAll(".tab-pane");
+
+  tabButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      // Убираем активный класс у всех кнопок
+      tabButtons.forEach((btn) => btn.classList.remove("active"));
+      // Убираем активный класс у всех панелей
+      tabPanes.forEach((pane) => pane.classList.remove("active"));
+
+      // Добавляем активный класс к текущей кнопке и панели
+      button.classList.add("active");
+      const tabId = button.getAttribute("data-tab");
+      document.getElementById(tabId).classList.add("active");
+    });
+  });
 });
